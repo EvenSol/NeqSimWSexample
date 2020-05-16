b1fb2d6acdd70eba1f4b74799bb8290b2018afa4
Even Solbraa <ESOL@equinor.com>
2020-05-16 14:25:17 +0200

    added java files

---
+++ src/main/java/com/neqsim/NeqSimWS/HelloController.java
@@ -0,0 +1,23 @@
+package com.neqsim.NeqSimWS;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("docker-java-app")
+public class HelloController {
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		return "docker-java-app is up and running: " + new Date();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
